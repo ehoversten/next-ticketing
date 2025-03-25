10325e17df6ee0c15565393e855b80c81c11b84c
Erik Hoversten <ehoversten@gmail.com>
2025-03-24 21:24:47 -0400

    testing CRUD operations with database

--- app/ticketpage/[id]/page.tsx
+++ app/ticketpage/[id]/page.tsx
@@ -1,13 +1,31 @@
-import React from 'react'
+import React from 'react';
+import { createClient } from '@/utils/supabase/server';
+
 
 // type TicketPageProps = {
 //   id: string;
 // }
 
-// function TicketPage({ params }) {
-function TicketPage() {
+// function TicketPage({ params: TicketPageProps }) {
+async function TicketPage({ params }) {
+
+    const { id } = await params;
+    const supabase = await createClient();
+    const { data: ticket, error } = await supabase
+                                            .from('tickets')
+                                            .select()
+                                            .eq('id', id);
+    console.log(ticket);
+
+    if(error) {
+        console.log("Error: ", error);
+    }
+
   return (
-    <div>TicketPage: ID -  { /*params.id */}</div>
+    <>
+        <div>TicketPage: ID -  { id }</div>
+        <p>{ JSON.stringify(ticket)}</p>
+    </>
   )
 }
 
